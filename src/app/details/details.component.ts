47629960f57f872a8d843e1a6bb670f9df9eeae4
Geoff Ingalls <ingallsGeoff@gmail.com>
2025-10-17 19:42:28 -0400

    basic functionality for the details page is complete. Stills needs Styles, safties, and comments

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+interface DetailResponse {
+  id: number;
+  name: string;
+  favorite_color: string;
+  quotes: { [likes: string]: string };
+}
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
+
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  detail: DetailResponse | null = null;
+  private apiUrl = 'http://localhost:5001/details/';
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.fetchDetails(id);
+    }
   }
 
+  fetchDetails(id: string): void {
+    this.http.get<DetailResponse>(`${this.apiUrl}${id}`).subscribe({
+      next: (data) => {
+        console.log('Details loaded:', data);
+        this.detail = data;
+      },
+      error: (err) => console.error('Error fetching details:', err)
+    });
+  }
+
+
+  getSortedQuotes(): [string, string][] {
+    return this.detail
+      ? Object.entries(this.detail.quotes).sort(
+        ([aLikes], [bLikes]) => Number(bLikes) - Number(aLikes)
+      )
+      : [];
+  }
 }
