f801f29e53523e2b1c2de71810699bb07fc97f3c
Geoff Ingalls <ingallsGeoff@gmail.com>
2025-10-17 19:14:20 -0400

    core search form and functionality added. Styles, safties, and comments to be added after D&D

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { DetailsComponent } from './details/details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,10 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+interface SearchParams {
+  term: string | null;
+  color: string | null;
+}
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
+
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchParams: SearchParams = {
+    term: null,
+    color: null
+  };
 
-  ngOnInit(): void {
+  results: any[] | null = null;
+
+  private apiUrl = 'http://localhost:5001/search';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.searchParams.color = '';
+    this.searchParams.term = '';
   }
 
+  getSearchResults(paramsObj: SearchParams): Observable<any> {
+    let params = new HttpParams();
+
+    if (paramsObj.term) {
+      params = params.set('term', paramsObj.term);
+    }
+
+    if (paramsObj.color) {
+      params = params.set('color', paramsObj.color);
+    }
+
+    return this.http.get<any>(this.apiUrl, { params });
+  }
+
+  onSearch(): void {
+    console.log('getSearchResults Called', this.searchParams);
+    this.getSearchResults(this.searchParams).subscribe({
+      next: (data) => {
+        this.results = Array.isArray(data.matches) ? data.matches : [data.matches]; // Ensure consistent display
+      },
+      error: (err) => {
+        console.error('Error:', err);
+        this.results = [];
+      }
+    });
+  }
 }
